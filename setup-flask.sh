b3877fb735a40daf0066543a328e282b2185e53b
atralex <atralexrg@gmail.com>
2024-02-13 17:07:24 +0100

    add setup-flask automation

---
+++ setup-flask.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Nombre del proyecto
+PROJECT_NAME=$1
+
+# Crear directorio del proyecto y navegar a él
+mkdir -p "$PROJECT_NAME"
+cd "$PROJECT_NAME"
+
+# Crear entorno virtual
+python3 -m venv venv
+
+# Activar entorno virtual
+source venv/bin/activate
+
+# Crear archivo requirements.txt
+touch requirements.txt
+
+# Instalar Flask
+pip install Flask
+
+# volcar librerias instaladas en requirements.txt
+pip freeze > requirements.txt
+
+# Crear main.py
+
+cat > main.py <<EOL
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+EOL
+
+# Crear archivo .gitignore
+touch .gitignore
+
+python3 main.py
